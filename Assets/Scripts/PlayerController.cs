e26cc4426fe7346cdadbbccb94d193e09ce58c27
Cristobal Ocampo Quintero <Cristobal Ocampo Quintero>
2016-04-05 01:13:44 -0500

    First version.Four walls and ball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb; //El Rigidbody
+    public float speed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(moveHorizontal,0.0f,moveVertical);
+        rb.AddForce(move*speed);
+	}
+}
